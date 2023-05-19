09060347a4651a817ca6ddd775259e64429e8f98
Juan Antonio Vivaldy <juananthonio23@gmail.com>
2023-05-19 12:11:43 +0700

    initial commit

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Juan Antonio Vivaldy Saragih'; // TODO: replace this
+
+const studentId = 'FE5126592'; // TODO: replace this
+
+const NetlifyDeployUrl = 'https://ruang-perpus.netlify.app/'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
